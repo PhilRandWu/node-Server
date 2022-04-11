ec960fdd8b7247948174459a601f70c9ab59365a
PhilRandWu <3378814052@qq.com>
2022-04-11 21:53:56 +0800

    第三方库解决跨域

--- Router/index.js
+++ Router/index.js
@@ -3,11 +3,12 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-04-02 22:28:11
- * @LastEditTime: 2022-04-11 21:40:43
+ * @LastEditTime: 2022-04-11 21:53:22
  * @LastEditors: PhilRandWu
  */
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
@@ -16,7 +17,19 @@ const staticRoot = path.resolve(__dirname, '../Public');
 app.use(express.static(staticRoot));
 
 // cors 跨域
-app.use(require('./middleWare/corsMiddleWare'));
+// app.use(require('./middleWare/corsMiddleWare')); // 手写
+
+// 第三方库
+const whiteList = ['http://127.0.0.1:5500', 'null'];
+app.use(cors({
+    origin(origin, callback) {
+        if (whiteList.includes(origin)) {
+            callback(null, origin);
+        } else {
+            callback(new Error("not allowed"));
+        }
+    }
+}))
 
 // 加入cookie-parser 中间件
 // 加入之后，会在req对象中注入cookies属性，用于获取所有请求传递过来的cookie
@@ -38,7 +51,7 @@ app.get('/', (req, res) => {
 
 // api
 app.use('/api/student', require('./api/student'));
-app.use('/api/admin',require('./api/admin'));
+app.use('/api/admin', require('./api/admin'));
 
 // 错误处理中间件
 app.use(require('./middleWare/errorMiddleWare'));
